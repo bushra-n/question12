02c409645b000ea1592e15ca4211398d2c271721
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-13 17:35:39 +0500

    Add files via upload

---
+++ ques12.js
@@ -0,0 +1,14 @@
+"use strict";
+// task 12 greetings msg to diff. people.
+// let Names:string [] = ["Bushra","Ammar","Rabia","Tehmina"];
+// let messAge:string="Welcome to the our organisation."
+// console.log(`Hello!, ${Names[0]},${messAge}`);
+// console.log(`Hello!, ${Names[1]},${messAge}`);
+// console.log(`Hello!, ${Names[2]},${messAge}`);
+// console.log(`Hello!, ${Names[3]},${messAge}`);
+// task 13 array of transportation and description template of each vehicle.
+// let transportation:string [] = ["Alto","Mercedes","Cultus Corrolla","City Honda"];
+// transportation.map ((items) => console.log (`I would like to own a ${items}`));
+// // task 14 invitation to guests for dinner
+let guestArr = ["Munazzah", "Maliha", "Tehmina", "Rabia"];
+guestArr.map((items) => console.log(`Dear ${items}, you are invited in dinner on Friday night.`));
